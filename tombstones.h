4b433f2aaa85ea89dd8f5cd61cb6984606c8daf8
Tim Robbins <86779745+Legatus727@users.noreply.github.com>
2022-04-27 11:50:30 -0400

    Final commit
    Added tombstone class in addition to pointer for compatibility

--- tombstones.h
+++ tombstones.h
@@ -1,73 +1,114 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+class Tombstone {
+public:
+    T* ptr;
+    int refCount;
+    bool RIP;
+
+    Tombstone<T>() : RIP(false), refCount(1), ptr(NULL) {}
+    Tombstone<T>(T* param) : ptr(param), refCount(1), RIP(false) {}
+    ~Tombstone<T>() {
+        if (ptr != NULL) {
+            delete ptr;
+            ptr = NULL;
+        }
+    }
+};
+
 template <typename T>
 class MyPointer {
+private:
+    Tombstone<T>* ts;
 public:
     // default constructor
-    MyPointer<T>(); 
-    
-    // copy constructor
-    MyPointer<T>(MyPointer<T>& param) { 
-        *data = new T;
-        *data = param.*data;
+    MyPointer<T>() {
+        ts = new Tombstone<T>();
     }
-    
+
+    // copy constructor
+    MyPointer<T>(MyPointer<T>& param) {
+        if (param.ts->RIP) {
+            std::cout << "Dangling reference error\n";
+            exit(1);
+        }
+        ts = param.ts;
+        ts->refCount++;
+    }
+
     // bootstrapping constructor
     // The parameter for the bootstrapping constructor should always be
     // a call to operator new.
-    MyPointer<T>(T* param){
-        *data = new T;
-        *data = param; 
+    MyPointer<T>(T* param) {
+        ts = new Tombstone<T>(param);
     }
-    
+
     // destructor
     ~MyPointer<T>() {
-        delete *data;
-    } 
+        ts->refCount--;
+    }
 
     // dereferencing
     T& operator*() const {
-        return &data;
-    } 
+        if (ts->RIP) {
+            std::cout << "Dangling reference error\n";
+            exit(1);
+        }
+        return *(ts->ptr);
+    }
 
     // field dereferencing
     T* operator->() const {
-        return *data;
-    } 
+        if (ts->RIP) {
+            std::cout << "Dangling reference error\n";
+            exit(1);
+        }
+        return ts->ptr;
+    }
 
     // assignment
     MyPointer<T>& operator=(const MyPointer<T>& param) {
-        free(this);
-        this.*data = new T;
-        this.*data = param.*data;
-    } 
-    
+        ts = param.ts;
+        ts->refCount++;
+        return *this;
+    }
+
     // delete pointed-at object
     // This is essentially the inverse of the new inside the call to
     // the bootstrapping constructor. It should delete the pointed-to
     // object (which should in turn call its destructor).
     friend void free(MyPointer<T>& param) {
-        delete param.data;
-    } 
-    
-    // equality comparisons:
-    // true iff MyPointer is null and int is zero
-    bool operator==(const MyPointer<T>& param) const {
-        return param.*data == NULL;
+        if (param.ts != NULL) {
+            if (param.ts->ptr != NULL)
+            {
+                delete param.ts->ptr;
+                param.ts->ptr = NULL;
+            }
+            param.ts->refCount--;
+            param.ts->RIP = true;
+        }
     }
-    bool operator==(const int param) const {
-        return param == 0;
-    }
-    
-    // false iff MyPointer is null and int is zero
-    bool operator!=(const MyPointer<T>& param) const {
-        return param.*data != NULL;
-    }
-    bool operator!=(const int param) const {
-        return param != 0;
-    }
-    
 
-private:
-    T data;
+    // equality comparisons:
+    bool operator==(const MyPointer<T>& param) const {
+        return ts->ptr == param.ts->ptr;
+    }
+    bool operator!=(const MyPointer<T>& param) const {
+        return ts->ptr != param.ts->ptr;
+    }
+
+    // true iff MyPointer is null and int is zero
+    bool operator==(const int param) const {
+        return  (ts->ptr == NULL && param == 0);
+    }
+
+    // false iff MyPointer is null and int is zero
+    bool operator!=(const int param) const {
+        return (ts->ptr != NULL || param != 0);
+    }
 };
 
 template <typename T>
